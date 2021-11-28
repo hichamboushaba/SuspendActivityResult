3518a64d081f9f51269bdd95c2a4742d1269c7a7
hicham <hicham.boushaba@gmail.com>
2021-11-28 21:02:17 +0100

    Update ExternalFilesViewModel

--- app/src/main/java/com/hicham/activityresult/files/ExternalFilesViewModel.kt
+++ app/src/main/java/com/hicham/activityresult/files/ExternalFilesViewModel.kt
@@ -1,12 +1,12 @@
 package com.hicham.activityresult.files
 
 import android.net.Uri
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
-import dev.hichamboushaba.suspendactivityresult.ActivityResultManager
 import com.hicham.activityresult.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dev.hichamboushaba.suspendactivityresult.ActivityResultManager
+import dev.hichamboushaba.suspendactivityresult.openDocument
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -34,10 +34,7 @@ class ExternalFilesViewModel @Inject constructor(
     fun pickFile() {
         viewModelScope.launch {
             savedStateHandle.set(IS_WAITING_FOR_FILE_KEY, true)
-            val uri = activityResultManager.requestResult(
-                ActivityResultContracts.OpenDocument(),
-                arrayOf("image/*")
-            )
+            val uri = activityResultManager.openDocument(arrayOf("image/*"))
             savedStateHandle.set(IMAGE_URI_KEY, uri)
             savedStateHandle.set(IS_WAITING_FOR_FILE_KEY, false)
 
