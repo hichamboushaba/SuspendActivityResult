cd09781987fccfaf75d024cb2d40460e99468da9
hicham <hicham.boushaba@gmail.com>
2021-12-06 13:41:43 +0100

    Add javadoc for permissions helpers

--- library/src/main/java/dev/hichamboushaba/suspendactivityresult/permission/PermissionManager.kt
+++ library/src/main/java/dev/hichamboushaba/suspendactivityresult/permission/PermissionManager.kt
@@ -5,7 +5,32 @@ interface PermissionManager {
         fun getInstance(): PermissionManager = PermissionManagerImpl
     }
 
+    /**
+     * Determine whether <em>you</em> have been granted a particular permission.
+     *
+     * @param permission The name of the permission being checked.
+     * @return a [PermissionStatus] to indicate the current status.
+     */
     fun hasPermission(permission: String): Boolean
+
+    /**
+     * Request a single permission.
+     *
+     * @param permission The name of the permission to be requested.
+     * @return a [PermissionStatus] to indicate the current status.
+     *
+     * @see [dev.hichamboushaba.suspendactivityresult.requestPermission]
+     */
     suspend fun requestPermission(permission: String): PermissionStatus
+
+    /**
+     * Request multiple permission.
+     *
+     * @param permissions the names of permissions to be requested.
+     * @return a [Map] containing the [PermissionStatus] of the request indexed by the
+     * permission name
+     *
+     * @see [dev.hichamboushaba.suspendactivityresult.requestPermissions]
+     */
     suspend fun requestPermissions(vararg permissions: String): Map<String, PermissionStatus>
 }
--- library/src/main/java/dev/hichamboushaba/suspendactivityresult/permission/PermissionStatus.kt
+++ library/src/main/java/dev/hichamboushaba/suspendactivityresult/permission/PermissionStatus.kt
@@ -1,5 +1,18 @@
 package dev.hichamboushaba.suspendactivityresult.permission
 
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.content.pm.PackageManager.PERMISSION_DENIED
+import android.app.Activity
+
 sealed class PermissionStatus
+
+/**
+ * @see [PERMISSION_GRANTED]
+ */
 object PermissionGranted : PermissionStatus()
+
+/**
+ * @see [PERMISSION_DENIED]
+ * @see [Activity.shouldShowRequestPermissionRationale]
+ */
 data class PermissionDenied(val shouldShowRationale: Boolean) : PermissionStatus()
